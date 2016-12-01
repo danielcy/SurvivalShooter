73ad0408f8d0c4c31118ef84f3e1a8891dc14f14
danielcy <448219947@qq.com>
2016-12-01 14:26:38 +0800

    Camera setup.

---
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform player;
+	public float smoothing = 5f;
+
+	private Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - player.position;
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 MoveToPosition = player.position + offset;
+		transform.position = Vector3.Lerp (transform.position, MoveToPosition, smoothing * Time.deltaTime);
+	}
+}
